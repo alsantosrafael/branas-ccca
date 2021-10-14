c2819dd1c83a666bda977787fdbdf93904527691
Rafael Almeida <rafael.profeng@gmail.com>
2021-10-14 19:02:49 -0300

    feat: ensure coupon checks if its expired or not

--- src/Coupon.ts
+++ src/Coupon.ts
@@ -1,6 +1,11 @@
-export default class Coupon{
+export default class Coupon {
 
-	constructor(readonly code: string, readonly percentage: number) {
-		
+	constructor(readonly code: string, readonly percentage: number, readonly expireDate?: Date) {}
+
+	isExpired (today: Date) {
+		if (this.expireDate) {
+			return this.expireDate.getTime() < today.getTime()
+		}
+		return false
 	}
 }

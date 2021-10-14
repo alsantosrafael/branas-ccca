bb8d76651a0a592a82694816aaf07363cd80d7e5
Rafael Almeida <rafael.profeng@gmail.com>
2021-10-14 19:11:07 -0300

    test: ensure a coupon never expires

--- test/Coupon.spec.ts
+++ test/Coupon.spec.ts
@@ -2,14 +2,20 @@ import Coupon from "../src/Coupon"
 
 describe('Coupon', () => {
 	test("Deve criar um cupom válido", () => {
-		const cupom = new Coupon("VALE20", 20, new Date("2021-10-14"))
-		const isExpired = cupom.isExpired(new Date("2021-10-10"))
-		expect(isExpired).toBeFalsy()
+		const cupom = new Coupon("VALE20", 20, new Date("2021-10-14"));
+		const isExpired = cupom.isExpired(new Date("2021-10-10"));
+		expect(isExpired).toBeFalsy();
 	})
 
 		test("Não deve criar um cupom inválido", () => {
-		const cupom = new Coupon("VALE20", 20, new Date("2021-09-14"))
-		const isExpired = cupom.isExpired(new Date("2021-10-10"))
-		expect(isExpired).toBeTruthy()
+			const cupom = new Coupon("VALE20", 20, new Date("2021-09-14"));
+			const isExpired = cupom.isExpired(new Date("2021-10-10"));
+			expect(isExpired).toBeTruthy();
+		})
+	
+		test("Deve criar um cupom que não expira nunca", () => {
+			const cupom = new Coupon("VALE20", 20);
+			const isExpired = cupom.isExpired(new Date("2021-10-10"));
+			expect(isExpired).toBeFalsy();
 	})
 })

b7a8642bed0e262e5359311494080c3c219fcc44
Rafael Almeida <rafael.profeng@gmail.com>
2021-10-18 19:34:38 -0300

    test: add getVolume method

--- test/Item.spec.ts
+++ test/Item.spec.ts
@@ -1,8 +1,15 @@
 import Item from "../src/Item"
 
+describe('Item', () => {
+	test("Deve criar um item", () => {
+		const item = new Item(1, "Instrumentos Musicais", "Guitarra", 1000);
+		expect(item.idItem).toBe(1);
+	});
 
-test("Deve criar um item", () => {
-	const item = new Item(1, "Instrumentos Musicais", "Guitarra", 1000);
-	expect(item.idItem).toBe(1);
+	test("deve criar um item e calcular o volume", () => {
+		const item = new Item(1, "Instrumentos Musicais", "Guitarra", 1000, 100, 30, 10 );
+		expect(item.getVolume()).toBe(0.03);
+
+	})
 
 })

4d0f725fda778db3a93efdd47cf16bfd0a9a1fa4
Rafael Almeida <rafael.profeng@gmail.com>
2021-10-18 19:34:32 -0300

    feat: add getVolume method

--- src/Item.ts
+++ src/Item.ts
@@ -1,5 +1,9 @@
 export default class Item {
-	constructor(readonly idItem: number, readonly category: string, readonly description: string, readonly price: number) {
-		
+
+	constructor(readonly idItem: number, readonly category: string, readonly description: string, readonly price: number, readonly width: number = 0, readonly height: number = 0, readonly length: number = 0){
+	}
+
+	getVolume() {
+		return this.width /100 * this.height/100 * this.length/100;
 	}
 }
